09078708800c7033bbc3f2240072ff2621fd0f95
Sarah <sarah.carlock86@gmail.com>
2024-05-21 12:03:23 -0500

    First tinkering

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './App.css';
+import './images/pattern-divider-desktop.svg';
+import './images/pattern-divider-mobile.svg';
+
+function App() {
+
+    return (
+   
+        <div className="App manrope-uniquifier">
+            <h1>Hello World</h1>
+                <img src="images/pattern-divider-desktop.svg" alt="pattern-divider-desktop" />
+        </div>
+
+    )
+};
+
+
+// w3-container w3-center manrope-uniquifier
