ca3968fecc7e6c1a47d05f421eedb264b788e978
Sarah <sarah.carlock86@gmail.com>
2024-05-21 12:57:18 -0500

    Start over, create react app to begin.

---
+++ advice-generator/src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+import './images/pattern-divider-desktop.svg';
+
+function App() {
+  return (
+    <div className="App manrope-uniquifier">
+      <header className="App-header">
+      <img src="images/pattern-divider-desktop.svg" alt="pattern-divider-desktop" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+
+
+        <div class="attribution">
+          Challenge by 
+          <a rel="noopener noreferrer" href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+          Coded by Sarah
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
